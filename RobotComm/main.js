ab2d2c40df62119d53f9e806225cba622396f67b
MichalMarchel84 <michal.marchel84@gmail.com>
2021-05-31 21:18:35 +0200

    Robot side disconnection handling

--- RobotComm/main.js
+++ RobotComm/main.js
@@ -1,40 +1,48 @@
-const h3 = document.querySelector("#h3");
-
 const port = chrome.runtime.connectNative("pl.marchel.robotcontrol");
+const signallingHost = "/app/signalling";
+const reportingHost = "/app/reports";
+const configuration = null;
+let stompClient;
+let timeout;
+let peerConnection;
+let dataChannel;
+let stream;
+
 port.onMessage.addListener(onNativeMessage);
 
-function onNativeMessage(message) {
-    h3.innerHTML = "Msg: <b>" + message.msg + "</b>";
-}
-
-//-----signalling------
-
-let stompClient = null;
-let robotName = "Robot1"
-let outputChannel = null;
 connect();
+
+//-------------Signalling----------------
+
+function onNativeMessage(message) {
+    //handling output from robot
+}
 
 function connect() {
 
-    const socket = new SockJS('http://192.168.1.12:8080/room');
+    const socket = new SockJS('http://192.168.1.12:8080/endpoint');
     stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-
-        stompClient.subscribe('/topic/Robot1', function (msg) {
-            onMessage(JSON.parse(msg.body));
+    stompClient.connect({},
+        function (frame) {
+            stompClient.subscribe('/app/authenticate',
+                function (msg) {
+                    onMessage(JSON.parse(msg.body));
+                },
+                {robotId: 1, robotPass: 123456});
+        }, function () {
+            finalizePeerConnection();
+            window.setTimeout(connect, 3000);
         });
-        stompClient.send("/app/connect", {}, JSON.stringify({"requestedId": robotName}))
-    });
-}
-
-function bind(msg) {
-    outputChannel = "/topic/" + msg.data;
-    startTransmission();
 }
 
 function send(type, data) {
 
-    stompClient.send(outputChannel, {}, JSON.stringify({"type": type, "data": data}));
+    stompClient.send(signallingHost, {"caller": "robot"}, JSON.stringify({"type": type, "data": data}));
+}
+
+function report(type, data) {
+
+    stompClient.send(reportingHost, {}, JSON.stringify({"type": type, "data": data}));
 }
 
 //-------------WebRTC code----------------
@@ -52,44 +60,76 @@ function onMessage(msg) {
         });
     } else if (msg.type === "answer") {
         peerConnection.setRemoteDescription(new RTCSessionDescription(msg.data));
-    } else if (msg.type === "binding") {
-        bind(msg);
+    } else if (msg.type === "start") {
+        timeout = window.setTimeout(signallingTimeout, 10000);
+        startTransmission();
     }
 }
 
-const configuration = null;
-const peerConnection = new RTCPeerConnection(configuration);
-let dataChannel;
+function initializePeerConnection() {
+    peerConnection = new RTCPeerConnection(configuration);
+    peerConnection.onicecandidate = function (event) {
+        if (event.candidate) {
+            send("candidate", event.candidate);
+        }
+    };
 
-peerConnection.onicecandidate = function (event) {
-    if (event.candidate) {
-        send("candidate", event.candidate);
+    peerConnection.onconnectionstatechange = function (event) {
+        switch (peerConnection.connectionState) {
+            case "connected":
+                onConnect();
+                break;
+            case "disconnected":
+                report("disconnect", "");
+                finalizePeerConnection();
+                break;
+            case "failed":
+                report("failed", "");
+                finalizePeerConnection();
+                break;
+        }
+    };
+}
+
+function finalizePeerConnection() {
+    if (stream != null) {
+        stream.getTracks().forEach(function (track) {
+            track.stop();
+        });
     }
-};
+    stream = null;
+    if (dataChannel != null) dataChannel.close();
+    if (peerConnection != null) peerConnection.close();
+    dataChannel = null;
+    peerConnection = null;
+}
+
+function onConnect() {
+    window.clearTimeout(timeout);
+    report("connect", "");
+}
+
+function signallingTimeout() {
+    report("timeout", "");
+}
 
 async function startVideo() {
     const constraints = {
-        video: {
-            width: {min: 800, ideal: 1200},
-            height: {min: 600, ideal: 720},
-            framerate: 5
-        },
-        audio: false
+        audio: false,
+        video: {width: 1280, height: 720, frameRate: 10}
     };
-
-
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
     peerConnection.addTrack(stream.getTracks()[0]);
-
 }
 
 async function startTransmission() {
+    initializePeerConnection();
     dataChannel = peerConnection.createDataChannel("dc");
     dataChannel.onerror = function (error) {
         console.log("Error:", error);
     };
     dataChannel.onclose = function () {
-        console.log("Data channel is closed");
+        console.log("Data channel closed");
     };
     dataChannel.onmessage = function (msg) {
         port.postMessage(JSON.parse(msg.data));
